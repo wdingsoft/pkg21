5c6143769eeb160ab824ab3f516c4584d7d44b7e
wdingsoft <wdingbox@gmail.com>
2023-01-25 08:57:03 -0500

    idxor

---
+++ idxobj.json.js
@@ -0,0 +1,200 @@
+var idxobj = {
+    "./": [
+        {
+            "idxor.html": 1455
+        },
+        {
+            "index.htm": 1
+        }
+    ],
+    "docs": [
+        {
+            "202206080819171917_OBI_Root_Study.pdf": 123195871
+        },
+        {
+            "Character_Wu.pdf": 1846343
+        },
+        {
+            "Feature_Extraction_and_Redesign_of_Bronze_Geometry.pdf": 675197
+        },
+        {
+            "KYC_0908_jiapukeci_hd_zi.pdf": 1636182
+        },
+        {
+            "OBI_Bronze_compare_Phd.pdf": 11603191
+        },
+        {
+            "OBI_DongBa_compare.doc": 15077376
+        },
+        {
+            "OBI_Hieroglyphics_Compare_master.pdf": 3011622
+        },
+        {
+            "OBI_Maya_Compare.pdf": 7421580
+        },
+        {
+            "OBI_Sumer_Cuniform_Compare.pdf": 2462856
+        },
+        {
+            "OBI_sacriface.pdf": 3769332
+        },
+        {
+            "OBI_weather.pdf": 1738818
+        },
+        {
+            "Turtles_StandingPerson_800BC.pdf": 6094775
+        },
+        {
+            "database_comparison.pdf": 1740142
+        },
+        {
+            "index.htm": 0
+        },
+        {
+            "pronouns7.pdf": 741101
+        },
+        {
+            "similarity.pdf": 834091
+        },
+        {
+            "whole_bones_statistics.pdf": 2397761
+        },
+        {
+            "《易经》“用”字考.pdf": 162850
+        },
+        {
+            "国内甲骨文数据库的比较研究_张芳.pdf": 1738208
+        },
+        {
+            "宁夏彭阳姚河塬商周遗址出土甲骨文.pdf": 1314422
+        },
+        {
+            "殷商甲骨文“册”“典”系字与图书_刘书芬.pdf": 1475649
+        },
+        {
+            "殷墟甲骨文“■”、“■”两字考辨_姚萱.pdf": 1191238
+        },
+        {
+            "殷墟甲骨文“其”语法位置与词性研究_张玉金.pdf": 1744071
+        },
+        {
+            "殷墟甲骨文“某人某”及其相关问题_赵鹏.pdf": 1064019
+        },
+        {
+            "殷墟甲骨文材料中整龟、整骨之统计_曲正清.pdf": 2390719
+        },
+        {
+            "甲骨文“之”字用法的再考察_李蕊.pdf": 3714953
+        },
+        {
+            "甲骨文“劌”字考究_吴祺.pdf": 2681517
+        },
+        {
+            "甲骨文“庚”字新考_吴祺.pdf": 2467730
+        },
+        {
+            "甲骨文“戉”字考究_吴祺.pdf": 3179464
+        },
+        {
+            "甲骨文“枝”“条”“萌”“耑”“倒”考辨_韩江苏.pdf": 1778188
+        },
+        {
+            "甲骨文“止”符字构形研究_何文芳.caj": 3692628
+        },
+        {
+            "甲骨文“止”符字构形研究_何文芳_第一节_研究综述_11_16.caj": 331125
+        },
+        {
+            "甲骨文“武”字的意象考释及其当代价值_尹永佩.pdf": 1845349
+        },
+        {
+            "甲骨文“臣”字形义新释——...代的职官泛称“亚”和“尹”_王进锋.pdf": 442982
+        },
+        {
+            "甲骨文、金文文字构形中“点”的系统研究_王文涵_1_5.caj": 1133416
+        },
+        {
+            "甲骨文与原始楔形文字新造合体字构字比较_白小丽.pdf": 4468408
+        },
+        {
+            "甲骨文与圣书字的对比之我观_崔姗姗.pdf": 888839
+        },
+        {
+            "甲骨文之前图画符号的研究_侯力丹.pdf": 4523416
+        },
+        {
+            "甲骨文形近字辨析举隅_罗树林.pdf": 832031
+        },
+        {
+            "甲骨文旧释“■”之字辨正_洪飏.pdf": 2087792
+        },
+        {
+            "甲骨文直线条研究.pdf": 2300503
+        },
+        {
+            "金文与甲骨文、战国文字的数字特点比较_金欣欣.pdf": 5071536
+        }
+    ],
+    "docs/img_totem": [
+        {
+            "Dragon_on_Ding.png.crdownload": 519762
+        },
+        {
+            "decorative-geometric-pattern-vector-chinese-260nw-311390198.webp": 16900
+        },
+        {
+            "download.png": 18354
+        },
+        {
+            "geometric_pattern1.png": 18354
+        },
+        {
+            "geometric_pattern2.jpeg": 36626
+        },
+        {
+            "geometric_pattern3.jpeg": 14400
+        },
+        {
+            "totem1.png": 62081
+        },
+        {
+            "totem2.png": 61629
+        },
+        {
+            "totem3.jpeg": 15495
+        },
+        {
+            "totem4.jpeg": 19195
+        },
+        {
+            "totem5.jpeg": 16695
+        },
+        {
+            "totem6.jpeg": 20092
+        },
+        {
+            "totem_metrics.png": 1914391
+        },
+        {
+            "weapons.png": 417031
+        }
+    ],
+    "docs/imgs": [],
+    "docs/imgs/gen_hai_meat": [
+        {
+            "gen_meat1.png": 1105119
+        },
+        {
+            "gen_meat2.png": 707049
+        },
+        {
+            "hai_01.png": 729514
+        },
+        {
+            "hai_02.png": 240159
+        },
+        {
+            "meat01.png": 258802
+        }
+    ]
+}
